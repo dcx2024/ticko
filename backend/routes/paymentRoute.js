fae9e8dfd245186bcd720c648eaf46cfd8b8764b
dcx <dcx2607@gmail.com>
2025-06-09 21:24:59 +0100

    This commit has fixed the routing for payment

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -1,16 +1,12 @@
 const { userAuth } = require('../authMIddleWare/authMiddleware')
-const{createSubAccount,initializePayment, verifyPayment,fetchBanks} = require('../Controllers/paymentController')
+const{createSubAccount ,initializePayment, verifyPayment,fetchBanks} = require('../Controllers/paymentController')
 const express = require('express')
 const router = express.Router()
-
+router.get('/fetchBanks', fetchBanks)
 router.post('/initialize', initializePayment)
 router.get('/verify/',verifyPayment)
-router.post('/createSubAcct', (req, res) => {
-  if (!req.body.bank) {
-    return res.status(400).json({ error: 'Bank code is required' }); // <-- maybe this is your error
-  }
-});
-router.get('/fetchBanks', fetchBanks)
+router.post('/createSubAcct', createSubAccount);
+
 
 
 

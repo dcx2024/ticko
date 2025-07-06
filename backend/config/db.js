851136f9257c86342a803e08d13e413c8e4a97e7
dcx2024 <166218735+dcx2024@users.noreply.github.com>
2025-07-06 21:32:42 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,24 @@
-const {Client} = require('pg')
+const {Pool} = require('pg')
 require ('dotenv').config()
 
-const db = new Client({
+/*const db = new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
-})
+})*/
+
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Important for Render
+  },
+});
+
+
+
 
 db.connect().then(()=>console.log('connected to postgresql')).catch((err)=>console.error('Database Connection error'))
 
